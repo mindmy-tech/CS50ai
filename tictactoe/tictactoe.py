a0155dbab3f2a3081bfa56610e8fe3f410aeef9b
Sabari <education@mindmy.tech>
2023-06-19 17:04:22 +0530

    player and action completed

--- tictactoe/tictactoe.py
+++ tictactoe/tictactoe.py
@@ -22,15 +22,29 @@ def player(board):
     """
     Returns player who has the next turn on a board.
     """
-    raise NotImplementedError
+    data = {}
+    for row in board:
+        for col in row:
+            if col in data:
+                data[col] += 1
+            else:
+                data[col] = 1
 
+    if data[None] % 2 != 0:
+        return X
+    else:
+        return O
 
 def actions(board):
     """
     Returns set of all possible actions (i, j) available on the board.
     """
-    raise NotImplementedError
-
+    actions_possible = set()
+    for x_index, row in enumerate(board):
+        for y_index, col in enumerate(row):
+            if col == EMPTY:
+                actions_possible.add(x_index, y_index)
+    return actions_possible
 
 def result(board, action):
     """
@@ -65,3 +79,9 @@ def minimax(board):
     Returns the optimal action for the current player on the board.
     """
     raise NotImplementedError
+
+
+## temp for testing 
+
+if __name__ == "__main__":
+    print(player(initial_state()))
